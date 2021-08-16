83b8786f0ec630c941a3c40ea18a3dfef3350a6f
Lelalorim <Liwonec@gmail.com>
2021-08-16 16:18:56 +0300

    feat(project): add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build'),
+  },
+  mode: 'development',
+  devServer: {
+    open: true,
+    writeToDisk: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+        exclude: /node_modules/,
+      }
+    ]
+  }
+};
