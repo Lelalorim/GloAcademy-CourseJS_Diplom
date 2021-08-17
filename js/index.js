02c297550440c4e75c5f1e47a96a2cb76aa0d086
Lelalorim <Liwonec@gmail.com>
2021-08-17 10:46:31 +0300

    feat(index.js style.css): add style: js-elem-hide. toggle elem opacity

--- js/index.js
+++ js/index.js
@@ -2,6 +2,20 @@
 
 import smoothScroll from './src/smoothScroll';
 
+const anchorSmoothScroll = document.querySelector('.smooth-scroll');
+anchorSmoothScroll.classList.add('js-elem-hide');
+
+document.addEventListener('scroll', ()=> {
+  const trackBlock = document.getElementById('benefits'),
+    requiredScrollValue = trackBlock.offsetTop;
+
+  if (window.pageYOffset > requiredScrollValue) {
+    anchorSmoothScroll.classList.remove('js-elem-hide');
+  } else {
+    anchorSmoothScroll.classList.add('js-elem-hide');
+  }
+});
+
 document.addEventListener('click', event=> {
   
   event.preventDefault();
